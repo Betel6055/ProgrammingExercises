0e087b0404dccb216e49ffb1384b72b73bb3800a
Santiago Castro Vargas <Santiago.Castro.Vargas@kyndryl.com>
2023-09-14 12:20:44 -0500

    correccion for LongestSubstringWithoutRepeatingCharacters

--- GeneralExercises/LongestSubstringWithoutRepeatingCharacters.py
+++ GeneralExercises/LongestSubstringWithoutRepeatingCharacters.py
@@ -1,26 +1,47 @@
+'''
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+'''
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # str_tmp = [i for i in s]
-        # len_s = len(s)
-        # for i in range(len_s):
-        #     for j in range(len_s):
-        #         if s[i] == str_tmp[j]:
-        #             print(f'{i}:{j}')
-        #     print('-'.center(20, '-'))
-        # print(str_tmp)
+        str_list = []
         str_tmp = ''
-        position = []
+        str_len = 0
+
         for i in range(len(s)):
-            if not(s[i] in str_tmp) and len(str_tmp) == 0:
+            if not(s[i] in str_tmp):
                 str_tmp += s[i]
-                position.append(i)
-            
+            else:
+                str_list.append(str_tmp)
+                str_tmp = ''
+                str_tmp += s[i]
 
-        print(str_tmp)
-        print(position)
-        return len(str_tmp)
+        str_list.append(str_tmp)
 
-s = "abcabcbb"
+        for i in str_list:
+            if str_len < len(i):
+                str_len = len(i)
+
+        return str_len
+
+s = "dvdf"
 object1 = Solution()
 print(object1.lengthOfLongestSubstring(s))
 
