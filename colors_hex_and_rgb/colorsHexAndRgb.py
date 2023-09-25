d6ce1cdb299a2721689741e27010d942f22f7efe
Betel6055 <107362789+Betel6055@users.noreply.github.com>
2023-09-24 19:10:02 -0500

    Modifying the code that converts colors from RGB to hexadecimal and vice versa

--- colors_hex_and_rgb/colorsHexAndRgb.py
+++ colors_hex_and_rgb/colorsHexAndRgb.py
@@ -8,29 +8,36 @@
  */
 """
 
-def rgbToHex(r = 0, g = 0, b = 0):
-    r_quotient = []
-    r_remainder = []
-    while True:
-        if len(r_quotient) > 0 and r_quotient[-1] > 16:
-            quotient = r_quotient[-1] // 16
-            remainder = r_quotient[-1] % 16
-            r_quotient.append(quotient)
-            r_remainder.append(remainder)
-        elif len(r_quotient) == 0:
-            quotient = r // 16
-            remainder = r % 16
-            r_quotient.append(quotient)
-            r_remainder.append(remainder)
-        else:
-            quotient = r_quotient[-1]
-            r_remainder.append(quotient)
-            break
+def rgbToHex(r = 0, g = 0, b = 0) -> hex:
+    hex_ = "#{:X}{:X}{:X}".format(r, g, b)
+    return hex_
 
-    print(r_quotient)
-    print(r_remainder)
+def hexToRgb(hex_) -> tuple:
+    r, g, b = int(hex_[1:3], 16), int(hex_[3:5], 16), int(hex_[5:7], 16)
+    rgb = (r, g, b)
+    return rgb
 
-rgbToHex(7000, 0, 0)
-# print(7000 % 16)
-# print(7000 / 16)
-# print(7000 // 16)
+while True:
+    try:
+        rgb = input('Enter the color rgb in format (r,g,b): ')
+        r, g, b = map(int, rgb.split(','))
+        assert 0 <= r <= 255, 'The value for r must be between 0 and 255'
+        assert 0 <= g <= 255, 'The value for g must be between 0 and 255'
+        assert 0 <= b <= 255, 'The value for b must be between 0 and 255'
+        break
+    except ValueError as ve:
+        print(f'Error: {ve}')
+        print('You must enter 3 values separated by commas')
+    except Exception as e:
+        print(f'Error: {e}')
+
+while True:
+    try:
+        hex_ = input('Enter the value hexadecimal in format (#000000): ')
+        assert len(hex_) == 7, 'The value does not contain the number of strings required'
+        break
+    except Exception as e:
+        print(f'Error: {e}')
+
+print(rgbToHex(r, g, b))
+print(hexToRgb(hex_))
